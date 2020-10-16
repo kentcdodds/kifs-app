cbff8929c52fe4901c7993cdbd94b1ed81b936b5
Kent C. Dodds <me+github@kentcdodds.com>
2020-10-16 13:22:43 -0600

    maybe this will work

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,5 @@
 global.fetch = require('node-fetch')
+const path = require('path')
 const express = require('express')
 const serverless = require('serverless-http')
 const {createRequestHandler: remix} = require('@remix-run/express')
@@ -9,6 +10,7 @@ app.use(express.static('public'))
 app.get(
   '*',
   remix({
+    root: path.join(__dirname, '..'),
     getLoadContext(req, res) {
       return {}
     },
