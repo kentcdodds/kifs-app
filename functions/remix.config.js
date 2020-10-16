5520a954996c3340d3e2f24244f4bed7ed620698
Kent C. Dodds <me+github@kentcdodds.com>
2020-10-16 13:14:50 -0600

    will this work maybe?

---
+++ functions/remix.config.js
@@ -0,0 +1,5 @@
+exports.browserBuildDirectory = '../public/build'
+exports.dataDirectory = '../loaders'
+exports.devServerPort = 8002
+exports.publicPath = '/build/'
+exports.serverBuildDirectory = '../server-build'
